47f97e189dc20dd074d8e43ce97b7ea6f42d1aea
montoyamoraga <montoyamoraga@gmail.com>
2017-03-27 19:30:02 -0400

    add test filescd

--- protest/__init__.py
+++ protest/__init__.py
@@ -5,3 +5,20 @@ import time
 import string
 import random
 from PIL import Image, ImageDraw, ImageFont
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def protest(subject):
+    protestSubject = subject
+
+def scrape_google():
+    # new driver using google chrome
+    driver = webdriver.Chrome()
+
+    # set the window size of the driver
+    driver.set_window_size(1200, 800)
+
+    # wait for 5 seconds
+    time.sleep(5)
+
+    driver.quit()
